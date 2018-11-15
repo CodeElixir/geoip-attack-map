d2084ce214bf5c5b8bbfb58d7e52de8cc6b71967
CodeElixir <14311A0431@sreenidhi.edu.in>
2018-11-16 04:32:05 +0530

    Modify sample syslog generater to match fortigate logs. This enusures us to test our modified code in DataServer without actually having log source

--- DataServer/syslog-gen.py
+++ DataServer/syslog-gen.py
@@ -1,45 +1,33 @@
 #!/usr/bin/python3
 
 import random, syslog
-from const import PORTMAP
 from sys import exit
 from time import sleep
+
 
 def main():
 
     port_list = []
-    type_attack_list = []
-
-    for port in PORTMAP:
-        port_list.append(port)
-        type_attack_list.append(PORTMAP[port])
-
+    
     while True:
         port = random.choice(port_list)
-        type_attack = random.choice(type_attack_list)
-        cve_attack = 'CVE:{}:{}'.format(
-                                random.randrange(1,2000),
-                                random.randrange(100,1000)
-                                )
 
-        rand_data = '{}.{}.{}.{},{}.{}.{}.{},{},{},{},{}'.format(
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            random.randrange(1, 256),
-                                                            port,
-                                                            port,
-                                                            type_attack,
-                                                            cve_attack
-                                                            )
-
-        syslog.syslog(rand_data)
-        print(rand_data)
+        rand_log = '''<189>date=2018-11-14 time=07:10:32 devname=BIBA-SURYO-FGT1 devid=FGVM020000071635 logid="0000000013" type="traffic" subtype="forward" level="notice" vd="root" logtime=1542159632 srcip={}.{}.{}.{} srcport={} srcintf="port1" srcintfrole="undefined" dstip={}.{}.{}.{} dstport={} dstintf="port2" dstintfrole="undefined" sessionid=1911411692 proto=6 action="deny" policyid=0 policytype="policy" service="tcp/47225" dstcountry="India" srccountry="Netherlands" trandisp="dnat" tranip=10.51.99.12 tranport=47225 duration=0 sentbyte=0 rcvdbyte=0 sentpkt=0 appcat="unscanned" crscore=30 craction=131072 crlevel="high"
+        '''.format(random.randrange(1, 256),
+                   random.randrange(1, 256),
+                   random.randrange(1, 256),
+                   random.randrange(1, 256),
+                   port,
+                   random.randrange(1, 256),
+                   random.randrange(1, 256),
+                   random.randrange(1, 256),
+                   random.randrange(1, 256),
+                   port
+                   )
+        syslog.syslog(rand_log)
+        #print(rand_log)
         sleep(1)
+
 
 if __name__ == '__main__':
     try:
